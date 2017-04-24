997b50e1fca896d34375bcb1823e5208a3ee1621
David Parra Perez <david.parra@outlook.com>
2017-04-24 11:03:18 +0200

    use ENV variable for github conf

--- userapi/start.sh
+++ userapi/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  
 
-git clone https://dparra0007@gitlab.com/dparra0007/W53-USERAPI-CONFIG.git
-source ./W53-USERAPI-CONFIG/env.sh
+git clone $SYSTEMCONF
+source $SYSTEMCONFFILE
 
 exec npm start
