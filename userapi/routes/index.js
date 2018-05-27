98e087fbbdd8e834e320c2d633577e9d9da32493
David Parra Perez <david.parra@outlook.com>
2018-05-27 17:39:21 +0200

    Prepare for dynamic config

--- userapi/routes/index.js
+++ userapi/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+process.env["NODE_CONFIG_DIR"] = "/usr/src/dynamic-conf/";
+var config = require('config');
+var extraMessage = config.get('Userapi.Extra.message');
+
 router.route('/users')
 
 .get(function(req, res) {
@@ -16,7 +20,7 @@ router.route('/users')
     var collection = db.get('userlist');
     collection.insert(req.body, function(err, result){
         res.send(
-            (err === null) ? { msg: process.env.USERAPI_ADDMESSAGE } : { msg: process.env.USERAPI_ERRMESSAGE }
+            (err === null) ? { msg: process.env.USERAPI_ADDMESSAGE + extraMessage } : { msg: process.env.USERAPI_ERRMESSAGE }
         );
     });
 });
@@ -28,7 +32,7 @@ router.route('/users/:id')
     var collection = db.get('userlist');
     var userToDelete = req.params.id;
     collection.remove({ '_id' : userToDelete }, function(err) {
-        res.send((err === null) ? { msg: process.env.USERAPI_DELMESSAGE } : { msg:'error: ' + process.env.USERAPI_ERRMESSAGE });
+        res.send((err === null) ? { msg: process.env.USERAPI_DELMESSAGE + extraMessage } : { msg:'error: ' + process.env.USERAPI_ERRMESSAGE });
     });
 });
 
