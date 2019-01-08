ff38c0191464b1240f875279cd5a7f175e0cef7a
david-iaggbs <david.parra@iaggbs.com>
2019-01-08 18:09:37 +0000

    fix eslint errors

--- userapi/app.js
+++ userapi/app.js
@@ -1,39 +1,43 @@
-var express = require('express');
+const express = require('express');
 
 const CLSContext = require('zipkin-context-cls');
 const zipkin = require('zipkin');
+
 const ctxImpl = new CLSContext('zipkin');
 const localServiceName = 'userapi';
-const tracer = new zipkin.Tracer({ctxImpl, recorder: new zipkin.ConsoleRecorder(), localServiceName});
+const tracer = new zipkin.Tracer({
+  ctxImpl,
+  recorder: new zipkin.ConsoleRecorder(),
+  localServiceName,
+});
 
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-// Database
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('userapi-db:27017/nodetest2');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const monk = require('monk');
 
-var routes = require('./routes/index');
-
-var app = express();
+const db = monk('userapi-db:27017/nodetest2');
 
 // instrument the server
 const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
-app.use(zipkinMiddleware({tracer}));
+
+const routes = require('./routes/index');
+
+const app = express();
+
+app.use(zipkinMiddleware({ tracer }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//logger
+// logger
 app.use(require('express-bunyan-logger')({
-  genReqId: function(req) {
-     return tracer.id;
-  }
+  genReqId() {
+    return tracer.id;
+  },
 }));
-/*var log = bunyan.createLogger({
+/* var log = bunyan.createLogger({
   name: 'userapi',
   serializers: { // add serializers for req, res and err
       req: bunyan.stdSerializers.req,
@@ -47,7 +51,7 @@ var express_logger = log.child({type: 'express', key: value});
 
 app.use(require('express-bunyan-logger')({
   logger: express_logger
-}));*/
+})); */
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -55,16 +59,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+app.use((req, res, next) => {
+  req.db = db;
+  next();
 });
 
 app.use('/userapi/1.0', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -74,23 +78,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status( err.code || 500 )
-    .json({
-      status: 'error',
-      message: err
-    });
+  app.use((err, req, res) => {
+    res.status(err.code || 500)
+      .json({
+        status: 'error',
+        message: err,
+      });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500)
-  .json({
-    status: 'error',
-    message: err.message
-  });
+    .json({
+      status: 'error',
+      message: err.message,
+    });
 });
 
 
--- userapi/routes/index.js
+++ userapi/routes/index.js
@@ -1,41 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+/* eslint-disable */
+const config = require('/usr/src/dynamic-conf/default.json');
+/* eslint-enable */
 
 router.route('/users/')
 
-.get(function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    collection.find({},{},function(e,docs){
-        res.json(docs);
+  .get((req, res) => {
+    const usersbd = req.db;
+    const collection = usersbd.get('userlist');
+    collection.find({}, {}, (e, docs) => {
+      res.json(docs);
     });
-})
+  })
 
-.post(function(req, res) {
-    if(req.query['clean'] === 'true') delete require.cache[require.resolve('/usr/src/dynamic-conf/default.json')];
-    var config = require('/usr/src/dynamic-conf/default.json');
-    var extraMessage = config.Userapi.Extra.message;
-    var db = req.db;
-    var collection = db.get('userlist');
-    collection.insert(req.body, function(err, result){
-        res.send(
-            (err === null) ? { msg: process.env.USERAPI_ADDMESSAGE + extraMessage } : { msg: process.env.USERAPI_ERRMESSAGE }
-        );
+  .post((req, res) => {
+    if (req.query.clean === 'true') delete require.cache[require.resolve('/usr/src/dynamic-conf/default.json')];
+    const extraMessage = config.Userapi.Extra.message;
+    const usersbd = req.db;
+    const collection = usersbd.get('userlist');
+    collection.insert(req.body, (err) => {
+      res.send((err === null) ? {
+        msg: process.env.USERAPI_ADDMESSAGE + extraMessage,
+      } : { msg: `error: ${process.env.USERAPI_ERRMESSAGE}` });
     });
-});
+  });
 
 router.route('/users/:id')
 
-.delete(function(req, res) {
-    if(req.query['clean'] === 'true') delete require.cache[require.resolve('/usr/src/dynamic-conf/default.json')];
-    var config = require('/usr/src/dynamic-conf/default.json');
-    var extraMessage = config.Userapi.Extra.message;
-    var db = req.db;
-    var collection = db.get('userlist');
-    var userToDelete = req.params.id;
-    collection.remove({ '_id' : userToDelete }, function(err) {
-        res.send((err === null) ? { msg: process.env.USERAPI_DELMESSAGE + extraMessage } : { msg:'error: ' + process.env.USERAPI_ERRMESSAGE });
+  .delete((req, res) => {
+    if (req.query.clean === 'true') delete require.cache[require.resolve('/usr/src/dynamic-conf/default.json')];
+    const extraMessage = config.Userapi.Extra.message;
+    const usersbd = req.db;
+    const collection = usersbd.get('userlist');
+    const userToDelete = req.params.id;
+    collection.remove({ _id: userToDelete }, (err) => {
+      res.send((err === null) ? {
+        msg: process.env.USERAPI_DELMESSAGE + extraMessage,
+      } : { msg: `error: ${process.env.USERAPI_ERRMESSAGE}` });
     });
-});
+  });
 
 module.exports = router;
